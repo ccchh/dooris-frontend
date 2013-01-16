95fa7042d987303600482c8fda05c4e56a7213a5
Felix Favre <1favre@informatik.uni-hamburg.de>
2013-01-16 17:50:29 +0100

    clean debug output

--- dooris.js
+++ dooris.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-  console.log('Page load successfull!');
+  //console.log('Page load successfull!');
   this.loadData = function() {
     $.get('json.php', function(data) {
         var jsonData = JSON.parse(data);
         jsonData['time'] = jsonData['time']*1000;
-        console.log(jsonData);
-        console.log(jsonData['door']);
-        console.log(jsonData['time']);
+        //console.log(jsonData);
+        //console.log(jsonData['door']);
+        //console.log(jsonData['time']);
         var status = jsonData['door'];
         var time = new Date(parseInt(jsonData['time']));
         var timeNow = $.now();
         var timeGoneBy = Math.round(((timeNow - time)/1000)/60);
-        console.log(timeGoneBy);
-        console.log(timeNow);
-        console.log(time);
-        console.log('Date: ' + time);
-        console.log('Status: ' + status);
+        //console.log(timeGoneBy);
+        //console.log(timeNow);
+        //console.log(time);
+        //console.log('Date: ' + time);
+        //console.log('Status: ' + status);
         if(status === '0') {
             $('#status').html("Please come in!").addClass('open');
         } else {
