0aaec1f68e912e08c5cb0d514cb9496b6a7aa17d
Julius Zeidler <julius@zeidlos.de>
2013-01-17 01:56:52 +0100

    Included new dooris.json together with a local switch for resolving problems with local development and jsonp. Also started to mess with the CSS. Current code may be broken.

--- dooris.js
+++ dooris.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
-  //console.log('Page load successfull!');
   this.loadData = function() {
-    $.get('json.php', function(data) {
-        var jsonData = JSON.parse(data);
-        jsonData['time'] = jsonData['time']*1000;
-        //console.log(jsonData);
-        //console.log(jsonData['door']);
-        //console.log(jsonData['time']);
-        var status = jsonData['door'];
-        var time = new Date(parseInt(jsonData['time']));
-        var timeNow = $.now();
-        var timeGoneBy = Math.round(((timeNow - time)/1000)/60);
-        //console.log(timeGoneBy);
-        //console.log(timeNow);
-        //console.log(time);
-        //console.log('Date: ' + time);
-        //console.log('Status: ' + status);
-        if(status === '0') {
-            $('#status').html("Please come in, we're open!").addClass('open');
-        } else {
-            $('#status').html("Sorry, we're closed.").addClass('closed');
-        };
-        $('#time').html("Last updated " + timeGoneBy + " Minutes ago.");
-      });
+    var pathname = window.location['host'];
+    if (pathname.toLowerCase().indexOf("localhost") >= 0) {
+      pathname = "json.php";
+      console.log('Localhost!');
+    } else {
+      pathname = "dooris.json";
     };
+    $.get(pathname, function(data) {
+      console.log(data);
+      var jsonData = JSON.parse(data);
+      jsonData['time'] = jsonData['time']*1000;
+      console.log(jsonData);
+      //console.log(jsonData['door']);
+      //console.log(jsonData['time']);
+      var status = jsonData['door'];
+      var time = new Date(parseInt(jsonData['time']));
+      var timeNow = $.now();
+      var timeGoneBy = Math.round(((timeNow - time)/1000)/60);
+      //console.log(timeGoneBy);
+      //console.log(timeNow);
+      //console.log(time);
+      //console.log('Date: ' + time);
+      //console.log('Status: ' + status);
+      if(status === '0') {
+          $('#status h1').html("Please come in, we're open!").addClass('open');
+      } else {
+          $('#status h1').html("Sorry, we're closed.").addClass('closed');
+      };
+      $('#time').html("Last updated " + timeGoneBy + " Minutes ago.");
+    });
+  };
   this.loadData();
   setInterval(this.loadData, 30*1000);
 });
--- json.php
+++ json.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "http://www.hamburg.ccc.de/~chris/dooris";
+$url = "http://www.hamburg.ccc.de/dooris/dooris.json";
 $ch = curl_init();
 $timeout = 5; // set to zero for no timeout
 curl_setopt ($ch, CURLOPT_URL, $url);
@@ -11,8 +11,6 @@ curl_close($ch);
 
 $door = trim(substr($x, 0, 1));
 $time = trim(substr($x, 1));
-
-$array = array("door" => $door, "time" => $time);
-echo json_encode($array);
-
+echo($door);
+echo($time);
 ?>
