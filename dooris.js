1de0e5fbe8d688384216da9a38476beacf1c4686
Julius Zeidler <julius@zeidlos.de>
2013-01-19 01:15:47 +0100

    Cleanup + fixes

--- dooris.js
+++ dooris.js
@@ -18,55 +18,60 @@ $(document).ready(function() {
   };
   var pathname = checkEnv();
 
-  // Returns diffrence between input timestamp and current time in minutes
-  var parseTimeDiffrence = function (time) {
-    var timeRec = time * 1000;
-    var timeNow = $.now();
-    var timeGoneBy = Math.round(((timeNow - timeRec)/1000)/60);
-    return timeGoneBy;
-  };
-
-  // Returns human readable string for input in minutes
+  // Returns human readable string for input in minutes, hours or days
   var parseTime = function(time) {
     var strTime = time;
-    if(strTime > 60) {
+    console.log(strTime);
+    if(strTime < 60) {
+      if(strTime === 1) {
+        strTime += ' minute';
+      }
+      strTime += ' minutes';
+    } else if(strTime > 60 || strTime < 1440) {
       strTime = Math.round(strTime/60);
-
       if(strTime === 1){
         strTime += ' hour';
       } else {
         strTime += ' hours';
-      };
+      }
     } else if (strTime > 1440) {
       strTime = Math.round(strTime/60/24);
       if(strTime === 1) {
         strTime += ' day';
       } else {
         strTime += ' days';
-      };
-    };
+      }
+    }
     return strTime;
+  };
+
+  // Returns diffrence between input timestamp and current time in minutes
+  var parseTimeDiffrence = function (time) {
+    var timeRec = time * 1000;
+    var timeNow = $.now();
+    return Math.round(((timeNow - timeRec)/1000)/60);
   };
 
   this.loadData = function() {
     $.getJSON(pathname, function(data) {
       var jsonData = data;
+      var statusDiv = $('#status');
       var status = jsonData['door']['status'];
       // Status Refresh all 5 minutes. If the status refresh is older than 5 minutes, there is a connection problem.
       if(parseTimeDiffrence(jsonData['door']['last_update']) > 6) {
         status = '-1';
       }
       if(status === '0') {
-        $('#status').html("Please come in, we're open!").addClass('open');
+        statusDiv.html("Please come in, we're open!").addClass('open');
       } else if (status ==='1') {
         // One client is the Freifunk-Router, so the minimum is greater 0.
         if (jsonData['router']['dhcp'] > 1) {
-          $('#status').html("There are " + jsonData['router']['dhcp'] + " DHCP Clients online, but the door is closed.").addClass('dhcp');
+          statusDiv.html("There are " + jsonData['router']['dhcp'] + " DHCP Clients online, but the door is closed.").addClass('dhcp');
         } else {
-          $('#status').html("Sorry, we're closed.").addClass('closed');
+          statusDiv.html("Sorry, we're closed.").addClass('closed');
         };
       } else {
-        $("#status").html("There is a connection Problem");
+        statusDiv.html("There is a connection Problem");
       };
       var time = parseTimeDiffrence(jsonData['door']['last_change']);
       time = parseTime(time);
